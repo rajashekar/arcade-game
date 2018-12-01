155179bee10202986b9f608be0ca7f27b57fde79
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2018-11-30 17:26:56 -0800

    refactor: ES6 conversion

--- js/app.js
+++ js/app.js
@@ -1,96 +1,103 @@
 // Enemies our player must avoid
-const Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  update(dt) {
     this.x = this.x > 500 ? getRandomX() : this.x + 50 * dt;
-    //console.log(this.x);
-};
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Player = function(x, y) {
+class Player {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-boy.png';
-};
+  }
 
-Player.prototype.update = function() {
-     // on touch of water go to start position
-     if(this.y<=0) {
-         this.x = 200;
-         this.y = 380;
-     }
-     // check for collisions
-     for (key in allEnemies) {
-         let enemyX = Math.floor(allEnemies[key].x);
-         let enemyY = Math.floor(allEnemies[key].y);
-        if (enemyX-75 <= this.x && this.x <= enemyX+75 && 
-            (enemyY-10 <= this.y && this.y <=enemyY+10)) {
-            console.log("collision found player", this.x, this.y);
-            console.log("collision found enemy", key, allEnemies[key]);
-            this.x = 200;
-            this.y = 380;
-        }
-     }
-};
+  update() {
+    this.resetOnTouchWater();
+    this.checkCollisions();
+  }
 
-Player.prototype.render = function() {
+  // on touch of water go to start position
+  resetOnTouchWater() {
+    if (this.y <= 0) {
+      this.x = 200;
+      this.y = 380;
+    }
+  }
+
+  // check for collisions
+  checkCollisions() {
+    for (let key in allEnemies) {
+      let enemyX = Math.floor(allEnemies[key].x);
+      let enemyY = Math.floor(allEnemies[key].y);
+      if (enemyX - 75 <= this.x && this.x <= enemyX + 75 &&
+        (enemyY - 10 <= this.y && this.y <= enemyY + 10)) {
+        console.log("collision found player", this.x, this.y);
+        console.log("collision found enemy", key, allEnemies[key]);
+        this.x = 200;
+        this.y = 380;
+      }
+    }
+  }
+
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
 
-Player.prototype.handleInput = function(input) {
+  handleInput(input) {
     console.log(input);
-    switch(input) {
-        case 'left' : this.x -= 20; break;
-        case 'right': this.x += 20; break;
-        case 'up'   : this.y -= 20; break;
-        case 'down' : this.y += 20; break;
+    switch (input) {
+      case 'left':
+        this.x -= 20;
+        break;
+      case 'right':
+        this.x += 20;
+        break;
+      case 'up':
+        this.y -= 20;
+        break;
+      case 'down':
+        this.y += 20;
+        break;
     }
     console.log(this.x, this.y, allEnemies);
-};
-
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const getRandomX = () => Math.floor(Math.random() * 400) - 399;
 const allEnemies = [];
-allEnemies.push(new Enemy(getRandomX(),60));
-allEnemies.push(new Enemy(getRandomX(),145));
-allEnemies.push(new Enemy(getRandomX(),230));
+allEnemies.push(new Enemy(getRandomX(), 60));
+allEnemies.push(new Enemy(getRandomX(), 145));
+allEnemies.push(new Enemy(getRandomX(), 230));
 
 // player initial location
 player = new Player(200, 380);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener('keyup', function (e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
